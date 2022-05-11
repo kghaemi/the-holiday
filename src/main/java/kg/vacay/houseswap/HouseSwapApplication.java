96067782a3eb29c83625712f5ae8a9ab2d866be4
Katie Ghaemi <katiepghaemi@gmail.com>
2022-05-10 20:48:18 -0600

    initial commit

---
+++ src/main/java/kg/vacay/houseswap/HouseSwapApplication.java
@@ -0,0 +1,13 @@
+package kg.vacay.houseswap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouseSwapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HouseSwapApplication.class, args);
+	}
+
+}
