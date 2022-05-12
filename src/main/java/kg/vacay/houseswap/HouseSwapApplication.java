376be3577012ab56d40706b52f7c6f1a00216373
Katie Ghaemi <katiepghaemi@gmail.com>
2022-05-12 09:58:13 -0600

    Stopping the automatic connection to the Database

--- src/main/java/kg/vacay/houseswap/HouseSwapApplication.java
+++ src/main/java/kg/vacay/houseswap/HouseSwapApplication.java
@@ -2,8 +2,9 @@ package kg.vacay.houseswap;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class HouseSwapApplication {
 
 	public static void main(String[] args) {
